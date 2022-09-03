d73056dcb8fa25e092097e63507f4a2a86ac6dc9
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-09-03 13:09:24 +0300

    Completed  assignment

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+	let reversedWord = reverseString(word);
+	return word === reversedWord;
 }
-
+function reverseString(word) {
+	return word.split("").reverse().join("");
+}
 /* 
   Add your pseudocode here
 */
@@ -12,14 +15,14 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+	// add your own custom tests in here
+	console.log("Expecting: true");
+	console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+	console.log("Expecting: false");
+	console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
